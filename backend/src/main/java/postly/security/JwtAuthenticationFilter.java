66ec6fb19598ee185e4399313cfcfeb054f7ac77
yelmach <yassineelmach0@gmail.com>
2025-09-16 18:01:37 +0100

    add security config

---
+++ backend/src/main/java/postly/security/JwtAuthenticationFilter.java
@@ -0,0 +1,22 @@
+package postly.security;
+
+import java.io.IOException;
+
+import org.springframework.lang.NonNull;
+import org.springframework.web.filter.OncePerRequestFilter;
+
+import jakarta.servlet.FilterChain;
+import jakarta.servlet.ServletException;
+import jakarta.servlet.http.HttpServletRequest;
+import jakarta.servlet.http.HttpServletResponse;
+
+public class JwtAuthenticationFilter extends OncePerRequestFilter {
+
+    @Override
+    protected void doFilterInternal(@NonNull HttpServletRequest request, @NonNull HttpServletResponse response,
+            @NonNull FilterChain filterChain)
+            throws ServletException, IOException {
+        throw new UnsupportedOperationException("Unimplemented method 'doFilterInternal'");
+    }
+
+}
