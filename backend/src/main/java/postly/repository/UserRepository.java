52072d40b9afc07a8b6870d33e6df395370e470a
yelmach <yassineelmach0@gmail.com>
2025-09-17 16:33:43 +0100

    update user repo

--- backend/src/main/java/postly/repository/UserRepository.java
+++ backend/src/main/java/postly/repository/UserRepository.java
@@ -3,12 +3,16 @@ package postly.repository;
 import java.util.Optional;
 
 import org.springframework.data.jpa.repository.JpaRepository;
+import org.springframework.data.jpa.repository.Query;
 import org.springframework.stereotype.Repository;
 
 import postly.entity.UserEntity;
 
 @Repository
 public interface UserRepository extends JpaRepository<UserEntity, Long> {
-
+    
+    @Query("SELECT u FROM UserEntity u where u.username = ?1 OR u.email = ?1")
     public Optional<UserEntity> findByUsernameOrEmail(String login);
+    public boolean existsByUsername(String username);
+    public boolean existsByEmail(String email);
 }
