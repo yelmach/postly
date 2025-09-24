e18c17eb956d6d41ddb7d87be731bf5c049e9696
yelmach <yassineelmach0@gmail.com>
2025-09-24 18:12:38 +0100

    apply style

--- frontend/src/app/app.routes.ts
+++ frontend/src/app/app.routes.ts
@@ -1,3 +1,14 @@
 import { Routes } from '@angular/router';
+import { guestGuard } from '@/guards/guest-guard';
 
-export const routes: Routes = [];
+export const routes: Routes = [
+  {
+    path: '',
+    canActivate: [guestGuard],
+    loadComponent: () => import('./pages/login/login').then(m => m.Login)
+  },
+  {
+    path: '**',
+    redirectTo: '/home'
+  }
+];
