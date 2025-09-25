10cd6c41dfc82ae04c6e0284e3638de092be976f
yelmach <yassineelmach0@gmail.com>
2025-09-25 18:12:21 +0100

    add auth interfaces & httpClient

--- frontend/src/app/app.ts
+++ frontend/src/app/app.ts
@@ -8,5 +8,5 @@ import { RouterOutlet } from '@angular/router';
   styleUrl: './app.scss'
 })
 export class App {
-  protected readonly title = signal('frontend');
+  protected readonly title = signal('Postly');
 }
---
+++ frontend/src/app/models/auth.ts
@@ -0,0 +1,27 @@
+export interface RegisterRequest {
+    firstName: String,
+    lastName: String,
+    username: String,
+    email: String,
+    password: String,
+    bio?: String,
+}
+
+export interface LoginRequest {
+    credentials: String,
+    password: String,
+}
+
+export interface AuthResponse {
+    token: String
+}
+
+export interface User {
+    firstName: String,
+    lastName: String,
+    username: String,
+    email: String,
+    role: 'USER' | 'ADMIN',
+    bio: String,
+    profileUrl: String,
+}
