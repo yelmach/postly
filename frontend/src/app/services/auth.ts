8574c930c0b3f3896b44ab9ef62899d0b9004339
yelmach <yassineelmach0@gmail.com>
2025-09-25 18:12:56 +0100

    start impl auth service

--- frontend/src/app/services/auth.ts
+++ frontend/src/app/services/auth.ts
@@ -1,8 +1,39 @@
-import { Injectable } from '@angular/core';
+import { AuthResponse, RegisterRequest, User } from "@/models/auth";
+import { HttpClient, HttpErrorResponse } from "@angular/common/http";
+import { inject, Injectable, signal } from "@angular/core";
+import { Router } from "@angular/router";
+import { environment } from "@environments/environment";
+import { catchError, Observable, tap, throwError } from "rxjs";
 
-@Injectable({
-  providedIn: 'root'
-})
-export class Auth {
-  
+@Injectable({ providedIn: "root" })
+class AuthService {
+    private readonly apiUrl = environment.apiUrl;
+
+    public currentUser = signal<User | null>(null);
+
+    http = inject(HttpClient);
+    router = inject(Router);
+
+    constructor() {
+
+    }
+
+    register(registerData: RegisterRequest): Observable<AuthResponse> {
+        return this.http.post<AuthResponse>(`${this.apiUrl}/auth/register`, registerData).pipe(
+            tap(this.handleAuthSuccess),
+            catchError(this.handleAuthError)
+        );
+    }
+
+    initializeAuthService() {
+
+    }
+
+    handleAuthSuccess(response: AuthResponse) {
+
+    }
+
+    handleAuthError(error: HttpErrorResponse): Observable<never> {
+        return throwError(() => "err");
+    }
 }
