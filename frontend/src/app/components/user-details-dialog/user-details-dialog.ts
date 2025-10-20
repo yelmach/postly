291588528288b32a2e170a1dae50affcd790d7cf
yelmach <yassineelmach0@gmail.com>
2025-10-20 10:35:53 +0100

    admin: add user details dialog

---
+++ frontend/src/app/components/user-details-dialog/user-details-dialog.ts
@@ -0,0 +1,43 @@
+import { Component, inject, Inject } from '@angular/core';
+import { CommonModule } from '@angular/common';
+import { MAT_DIALOG_DATA, MatDialogModule, MatDialogRef } from '@angular/material/dialog';
+import { MatButtonModule } from '@angular/material/button';
+import { MatIconModule } from '@angular/material/icon';
+import { MatChipsModule } from '@angular/material/chips';
+import { MatDividerModule } from '@angular/material/divider';
+import { AdminUser } from '@/models/admin';
+
+export interface UserDetailsDialogData {
+  user: AdminUser;
+}
+
+@Component({
+  selector: 'app-user-details-dialog',
+  imports: [
+    CommonModule,
+    MatDialogModule,
+    MatButtonModule,
+    MatIconModule,
+    MatChipsModule,
+    MatDividerModule,
+  ],
+  templateUrl: './user-details-dialog.html',
+  styleUrl: './user-details-dialog.scss',
+})
+export class UserDetailsDialog {
+  private dialogRef = inject(MatDialogRef<UserDetailsDialog>);
+
+  constructor(@Inject(MAT_DIALOG_DATA) public data: UserDetailsDialogData) {}
+
+  get fullName(): string {
+    return `${this.data.user.firstName} ${this.data.user.lastName}`;
+  }
+
+  onViewProfile() {
+    window.open(`/profile/${this.data.user.username}`, '_blank');
+  }
+
+  onClose() {
+    this.dialogRef.close();
+  }
+}
