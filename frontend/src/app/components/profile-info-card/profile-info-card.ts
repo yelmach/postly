1f0fbe4ffd55af840c709c9f220bdf0c03f31bb3
yelmach <yassineelmach0@gmail.com>
2025-10-29 12:05:42 +0100

    improve side bar

--- frontend/src/app/components/profile-info-card/profile-info-card.ts
+++ frontend/src/app/components/profile-info-card/profile-info-card.ts
@@ -2,12 +2,13 @@ import { Component, input } from '@angular/core';
 import { CommonModule } from '@angular/common';
 import { MatCardModule } from '@angular/material/card';
 import { MatIconModule } from '@angular/material/icon';
+import { MatButtonModule } from '@angular/material/button';
 import { Router } from '@angular/router';
 import { User } from '@/models/user';
 
 @Component({
   selector: 'app-profile-info-card',
-  imports: [CommonModule, MatCardModule, MatIconModule],
+  imports: [CommonModule, MatCardModule, MatIconModule, MatButtonModule],
   templateUrl: './profile-info-card.html',
   styleUrl: './profile-info-card.scss',
 })
